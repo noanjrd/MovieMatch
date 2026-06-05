baa96a4d3ef9d9d395c1a24a3b2c3bfc19228261
Noan <noan.pujol@gmail.com>
2026-06-05 17:08:27 +0200

    table arrangement started

--- server/train_model.py
+++ server/train_model.py
@@ -1,14 +1,24 @@
 import pandas as pd
-
+import sys
 
 def main():
-    try:
-        movies = pd.read_csv("./datasets/links.csv")
-        ratings = pd.read_csv("./datasets/ratings.csv")
-        print(movies.head)
-    except:
-        print("Error")
-        exit(1)
+    argv = sys.argv
+    print(argv)
+    user_id = int(argv[1])
+    movies = pd.read_csv("./datasets/links.csv")
+    ratings = pd.read_csv("./datasets/ratings.csv")
+    # print(movies.head)
+    user_movies = ratings.pivot(
+        index="userId",
+        columns="movieId",
+        values="rating"
+    )
+    row_of_user = user_movies.loc[user_id]
+    # print(row_of_user)
+    seen_movies = row_of_user.dropna().index
+    user_movies = user_movies[seen_movies]
+    print(user_movies)
+    print(user_movies.iloc[user_id])
     return
 
 if __name__ == "__main__":
