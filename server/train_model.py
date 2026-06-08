773645173711a6d67d906d451189243d2e63f011
Noan <noan.pujol@gmail.com>
2026-06-08 16:34:59 +0200

    Cosine Similarity started

--- server/train_model.py
+++ server/train_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import sys
 
 def main():
@@ -14,11 +15,35 @@ def main():
         values="rating"
     )
     row_of_user = user_movies.loc[user_id]
-    # print(row_of_user)
     seen_movies = row_of_user.dropna().index
+    print(seen_movies)
     user_movies = user_movies[seen_movies]
     print(user_movies)
-    print(user_movies.iloc[user_id])
+    user_a = user_movies.loc[user_id]
+    user_b = user_movies
+    for id, row in user_movies.iterrows():
+        # print(id)
+        # print(row.notna())
+        if id == user_id:
+            continue
+        common  = row.notna() & user_a.notna()
+        com_t = common[common == True]
+        
+        # print(com_t)
+        # print(common[common["movieId"] == True])
+        # print(com_t)
+        if com_t.size < 10:
+            continue
+        dot_res = np.dot(row[common], user_a[common])
+        l = np.linalg.norm(row[common])
+        l2 = np.linalg.norm(user_a[common])
+        cos = dot_res / (l * l2)
+        print(id, cos)
+        
+        # print(common)
+        
+    # common = user_a.notna() & user_b.notna()
+    # print(common)
     return
 
 if __name__ == "__main__":
